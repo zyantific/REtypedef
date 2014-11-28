4baeca7175b6c4e40c5e7789bc3b5fa9bce48ddd
Ende! <ende1337@gmail.com>
2014-11-28 18:12:27 +0100

    increased version to 1.0.1

--- Config.hpp
+++ Config.hpp
@@ -26,8 +26,8 @@
 #define CONFIG_HPP
 
 #define MAKE_PLUGIN_VERSION(maj, min, rev)  ((maj << 16) | (min << 8) | rev)
-#define PLUGIN_TEXTUAL_VERSION  "v1.0.0"
-#define PLUGIN_VERSION          MAKE_PLUGIN_VERSION(1, 0, 0)
+#define PLUGIN_TEXTUAL_VERSION  "v1.0.1"
+#define PLUGIN_VERSION          MAKE_PLUGIN_VERSION(1, 0, 1)
 
 #define PLUGIN_NAME             "REtypedef"
 
